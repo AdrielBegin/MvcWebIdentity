2ee56dcae1506d351c9d364f77817f52be0ce4c2
AdrielBegin <adriel.pemi@gmail.com>
2023-10-06 10:20:28 -0400

    Add project files.

---
+++ MvcWebIdentity/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcWebIdentity.Entities;
+
+namespace MvcWebIdentity.Context
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Aluno> Alunos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Aluno>().HasData(new Aluno
+            {
+                AlunoId = 1,
+                Nome = "Adriel",
+                Email = "adriel@gmail.com",
+                Idade = 24,
+                Curso = "ADS"
+            });
+        }
+    }
+}
